5c1fed1bdc963d0f610218214f82342c306a5873
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-05-05 21:39:32 +0200

    Reset changes

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
 window.addEventListener("load", function () {
     const vueContainer = new Vue({
         el: '#vueContainer',
