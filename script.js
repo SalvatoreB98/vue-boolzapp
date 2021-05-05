6fe41ca3150d58b26a574a4f4111bed27ae70abe
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-05-05 13:04:20 +0200

    Delete scroll

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
-window.addEventListener("load",function(){
+window.addEventListener("load", function () {
     const vueContainer = new Vue({
         el: '#vueContainer',
-        data:{
+        data: {
             usersList: globalUsersList,
-            activeChat : 0,
+            activeChat: 0,
             isChatOpen: false,
             textInput: "",
             date: ''
         },
-        computed:{
+        computed: {
 
         },
-        methods:{
-            setActiveChat(id){
+        methods: {
+            setActiveChat(id) {
                 this.activeChat = id;
             },
-            getTimeHm(date){
+            getTimeHm(date) {
                 return moment(date).format("HH:mm")
             },
-            chatOpen(){
+            chatOpen() {
                 this.isChatOpen = true;
             },
-            chatClose(){
+            chatClose() {
                 this.isChatOpen = false;
             },
-            sendMessage(){
-                let date = moment()
-               this.usersList[this.activeChat].messages.push({
-                                                                date: date, 
-                                                                text: this.textInput,  
-                                                                status: 'sent'})
+            sendMessage() {
+                if(this.textInput!==""){
+                    let date = moment()
+                this.usersList[this.activeChat].messages.push({
+                    date: date,
+                    text: this.textInput,
+                    status: 'sent'
+                });
+                this.textInput = '';
+                this.serverAnswer();
+                }
+            },
+            serverAnswer() {
+                setTimeout(() => {
+                    let date = moment()
+                    this.usersList[this.activeChat].messages.push({
+                        date: date,
+                        text: "OK!",
+                        status: 'received'
+                    });
+                }, 1000);
+
             }
         }
     })
