06db8780fd696e6058b059fa6a3926de21737f3b
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-05-05 21:38:17 +0200

    prova 2

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-window.addEventListener("load", function () {
-    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+window.addEventListener('resize', () => {
+    // We execute the same script as before
     let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+window.addEventListener("load", function () {
     const vueContainer = new Vue({
         el: '#vueContainer',
         data: {
